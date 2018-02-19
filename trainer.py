efaa9b9345e90ba77e2191a0f3bbb5e3aa493f82
Patric Liu <patric.f.liu@gmail.com>
2018-02-19 12:54:02 -0600

    update

--- trainer.py
+++ trainer.py
@@ -1,3 +1,15 @@
+''' Trainer
+
+Randomly Initializes and then trains Athena from scratch using evolutionary
+strategies
+
+Future Functionality:
+Pretrain Athena using gradient descent and hand-curated input-output pairs
+before running through evolution. Saving pretrained networks and then importing
+them should save on initial training time. This runs the risk of not fully 
+exploring the parameter space and getting stuck at the same local minimums
+'''
+
 import network_self
 import numpy as np
 import evolve
@@ -5,13 +17,11 @@ import time
 import all_parameters
 para = all_parameters.Parameters()
 
+# keep track of computation time
 start_time = time.time()
 
-# specify desired network shape
-net_sizes = para.sizes
-
 # initialize new random network/ option to import existing should be added
-Athena = network_self.Network(net_sizes)
+Athena = network_self.Network(para.sizes)
 
 # exports network to evolution class
 Train = evolve.Evolution(parameters = Athena.parameters)
